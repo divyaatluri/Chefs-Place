94f0ef89b99644a1cfb89b679599d945e7c37e33
Divya Atluri <divy.atluri1992@gmail.com>
2024-02-10 15:04:01 +0800

    Added top rated restaurnats filter functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Restaurant from "./Restaurant";
 import "./Body.css";
 import { restaurants } from "../utils/mockData";
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(restaurants);
   return (
     <div className="body-container">
-      <div className="search">Search</div>
+      <div className="btn-container">
+        <button
+          className="topRated-btn"
+          onClick={() => {
+            const filteredlistOfRestaurants = listOfRestaurants.filter(
+              (res) => res.info.avgRating >= 4.5
+            );
+            setListOfRestaurants(filteredlistOfRestaurants);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {restaurants.map((restaurant) => {
+        {listOfRestaurants.map((restaurant) => {
           return <Restaurant key={restaurant.info.id} resData={restaurant} />;
         })}
         <Restaurant />
