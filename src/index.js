1a143bf2e6b998f7e8aeb0673a39c71d90c70034
Divya Atluri <divy.atluri1992@gmail.com>
2024-03-18 19:51:31 +0800

    Routing to home page and About Us page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import ReactDOM from "react-dom";
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import AboutUs from "./components/AboutUs";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <AboutUs />,
+  },
+]);
+
+ReactDOM.render(
+  <RouterProvider router={appRouter} />,
+  document.getElementById("root")
+);
